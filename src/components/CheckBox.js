69de7ba80bdd83d6939ec2a768133b1fe5b85e45
Tom Lin <tom@js4.af.dev>
2017-07-05 13:25:44 -0400

    refactor checkbox component into functional

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,30 +1,22 @@
 import React, { Component } from 'react';
 import Icon from  'react-native-vector-icons/MaterialIcons';
 
-class CheckBox extends Component {
-  constructor(props) {
-    super(props);
-  }
+export default function CheckBox({ data, color, onCheckBoxPressed }) {
+  const iconName = data.completed ? 'check-box' : 'check-box-outline-blank';
+  const checkBoxColor = color || '#000';
 
-  render() {
-    let iconName = this.props.data.completed ? 'check-box' : 'check-box-outline-blank';
-    let color = this.props.color || '#000';
-
-    return (
-      <Icon.Button
-        data={this.props.data}
-        name={iconName}
-        backgroundColor='rgba(0,0,0,0)'
-        color={color}
-        underlayColor='rgba(0,0,0,0)'
-        size={20}
-        iconStyle={{marginLeft: -10, marginRight: 0}}
-        activeOpacity={1}
-        borderRadius={5}
-        onPress={this.props.onCheckBoxPressed}
-      />
-    );
-  }
+  return (
+    <Icon.Button
+      data={data}
+      name={iconName}
+      backgroundColor='rgba(0,0,0,0)'
+      color={checkBoxColor}
+      underlayColor='rgba(0,0,0,0)'
+      size={20}
+      iconStyle={{marginLeft: -10, marginRight: 0}}
+      activeOpacity={1}
+      borderRadius={5}
+      onPress={onCheckBoxPressed}
+    />
+  );
 }
-
-module.exports = CheckBox;
